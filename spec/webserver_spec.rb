0696badb71d8e69add5a99dec3b83152e7423d4b
tim <tim@tims-MacBook-Air.local>
2017-05-19 13:57:15 +0800

    Refactor webpage to generic hello world

--- spec/webserver_spec.rb
+++ spec/webserver_spec.rb
@@ -3,35 +3,35 @@ require "docker"
 
 describe "I am logged onto the webserver" do
   container = nil
-  
+
   before(:all) do
     set :backend, :exec
   end
-  
+
   describe "MyApp" do
-  
+
     describe docker_container('myapp') do
       it { should be_running }
     end
-  
+
     describe port(8080) do
       it { should be_listening }
     end
-  
+
     describe command("curl http://localhost:8080") do
-      its(:stdout) { should match /Hello CukeUp Australia/ }    
+      its(:stdout) { should match /Hello World/ }    
     end
-    
+
   end
-  
+
   describe "Routing" do
     describe "Internet connectivity is enabled" do
       describe host('8.8.8.8') do
         it { should be_reachable.with( :port => 53 ) }
-      end     
+      end
       describe host('www.google.com') do
         it { should be_reachable.with( :port => 443 ) }
-      end     
+      end
     end
   end
 
